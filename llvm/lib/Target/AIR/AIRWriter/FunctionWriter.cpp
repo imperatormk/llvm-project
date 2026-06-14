e3ee50dd2d6633e837aadf1ec14b458d83e831b5
imperatormk <darko.simonovski@hotmail.com>
2026-06-14 23:12:45 +0200

    [AIR] writer: RPO block emission + null-ptr access-type fix
    Twin of the triton-ext Metal AIR-writer fix. (1) FunctionWriter emits basic
    blocks in reverse-post-order so the relative value-ID encoding never sees a
    forward reference (textual order underflowed -> Invalid record). (2)
    BitcodeEmitter fixAccessTypeMismatch retypes a ConstantPointerNull pointer
    operand via identity bitcast to the access type, like inttoptr. Fixes the
    inductor triton_mm materializeAll/Invalid-record crash.

--- llvm/lib/Target/AIR/AIRWriter/FunctionWriter.cpp
+++ llvm/lib/Target/AIR/AIRWriter/FunctionWriter.cpp
@@ -17,6 +17,8 @@
 #include "MetadataWriter.h"
 #include "AIRConstraints.h"
 #include "ValueEnumerator.h"
+#include "llvm/ADT/PostOrderIterator.h"
+#include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/Bitcode/LLVMBitCodes.h"
 #include "llvm/Bitstream/BitstreamWriter.h"
 #include "llvm/IR/Instructions.h"
@@ -36,6 +38,19 @@ void emitConstantsBlock(BitstreamWriter &W, ValueEnumerator &E,
 void emitFunctionBlock(BitstreamWriter &W, const Function &F,
                        ValueEnumerator &E, const MetadataEnumerator &MD) {
   W.EnterSubblock(bitc::FUNCTION_BLOCK_ID, 5);
+
+  SmallVector<const BasicBlock *, 8> BBOrder;
+  {
+    SmallPtrSet<const BasicBlock *, 8> Seen;
+    if (!F.isDeclaration()) {
+      for (const BasicBlock *BB : ReversePostOrderTraversal<const Function *>(&F))
+        if (Seen.insert(BB).second)
+          BBOrder.push_back(BB);
+      for (const BasicBlock &BB : F)
+        if (Seen.insert(&BB).second)
+          BBOrder.push_back(&BB);
+    }
+  }
 
   // Build local value ID map
   DenseMap<const Value *, unsigned> LocalMap;
@@ -57,8 +72,8 @@ void emitFunctionBlock(BitstreamWriter &W, const Function &F,
         FuncConsts.push_back(C);
       }
   };
-  for (auto &BB : F)
-    for (auto &I : BB) {
+  for (const BasicBlock *BB : BBOrder)
+    for (auto &I : *BB) {
       for (auto &Op : I.operands())
         CollectConst(Op);
       // The shuffle mask is not an operand in-memory but is referenced by
@@ -68,8 +83,8 @@ void emitFunctionBlock(BitstreamWriter &W, const Function &F,
     }
 
   // Instruction results
-  for (auto &BB : F)
-    for (auto &I : BB)
+  for (const BasicBlock *BB : BBOrder)
+    for (auto &I : *BB)
       if (!I.getType()->isVoidTy())
         LocalMap[&I] = NextID++;
 
@@ -93,19 +108,14 @@ void emitFunctionBlock(BitstreamWriter &W, const Function &F,
   };
 
   // DECLAREBLOCKS
-  unsigned NumBBs = 0;
-  for (auto &BB : F)
-    (void)BB, NumBBs++;
-  SmallVector<uint64_t, 1> DV = {NumBBs};
+  SmallVector<uint64_t, 1> DV = {BBOrder.size()};
   W.EmitRecord(bitc::FUNC_CODE_DECLAREBLOCKS, DV);
 
   // Function constants
   emitConstantsBlock(W, E, FuncConsts, 5);
 
   // BB index helper
-  SmallVector<const BasicBlock *, 8> BBList;
-  for (auto &BB : F)
-    BBList.push_back(&BB);
+  SmallVector<const BasicBlock *, 8> BBList(BBOrder.begin(), BBOrder.end());
   auto BBIdx = [&](const BasicBlock *BB) -> unsigned {
     for (unsigned I = 0; I < BBList.size(); I++)
       if (BBList[I] == BB)
@@ -118,8 +128,8 @@ void emitFunctionBlock(BitstreamWriter &W, const Function &F,
   // an InstructionList in parallel) can index it via Record[0].
   SmallVector<std::pair<unsigned, const Instruction *>, 8> Attached;
   unsigned EmittedIdx = 0;
-  for (auto &BB : F) {
-    for (auto &I : BB) {
+  for (const BasicBlock *BB : BBOrder) {
+    for (auto &I : *BB) {
       SmallVector<uint64_t, 16> V;
 
       if (auto *BO = dyn_cast<BinaryOperator>(&I)) {
